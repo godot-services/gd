c47dd6c8c3c169a1d00ee5cd27d0777e7bc7433a
MRW Neundorf <ninevillage@gmail.com>
2025-03-04 22:01:55 +0100

    Add more specs for internal/editor/editor.go to test several cases
    This might improve test coverage for given file :eyes:

--- internal/editor/editor_test.go
+++ internal/editor/editor_test.go
@@ -1,6 +1,7 @@
 package editor_test
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"testing"
@@ -9,7 +10,7 @@ import (
 	"github.com/godot-services/gd/internal/editor/testdata"
 )
 
-func TestNewEditor(t *testing.T) {
+func TestNewEditorWithValidLocation(t *testing.T) {
 	t.Parallel()
 
 	cwd, err := os.Getwd()
@@ -22,5 +23,64 @@ func TestNewEditor(t *testing.T) {
 	_, err = editor.NewEditor(p)
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestNewEditorWithNotAbsoluteLocation(t *testing.T) {
+	t.Parallel()
+
+	p := filepath.Join("testdata", testdata.MockVersion)
+
+	_, err := editor.NewEditor(p)
+	if err != editor.ErrLocationMustBeAbs {
+		t.Fatal("expected", editor.ErrLocationMustBeAbs)
+	}
+}
+
+func TestNewEditorWithLocationToDir(t *testing.T) {
+	t.Parallel()
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p := filepath.Join(cwd, "testdata")
+
+	_, err = editor.NewEditor(p)
+	if err != editor.ErrLocationMustBeAFile {
+		t.Fatal("expected", editor.ErrLocationMustBeAFile, "but got", err)
+	}
+}
+
+func TestNewEditorWithUnknownLocation(t *testing.T) {
+	t.Parallel()
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p := filepath.Join(cwd, "unknown")
+
+	_, err = editor.NewEditor(p)
+	if !errors.Is(err, os.ErrNotExist) {
+		t.Fatal("expected", os.ErrNotExist, "but got", err)
+	}
+}
+
+func TestNewEditorWithLocationToNonExecutable(t *testing.T) {
+	t.Parallel()
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p := filepath.Join(cwd, "testdata", testdata.MockSimpleFile)
+
+	_, err = editor.NewEditor(p)
+	if err == nil {
+		t.Fatal("expected execution error for non application but got no error")
 	}
 }
---
+++ internal/editor/testdata/mock.go
@@ -0,0 +1,3 @@
+package testdata
+
+const MockSimpleFile = "mock_simple_file.txt"
